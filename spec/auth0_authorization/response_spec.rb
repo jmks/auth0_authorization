170dc69aaa4a3707ef1cb3df1db4648ecdff3a48
Jason Schweier <jasons@nulogy.com>
2021-05-19 19:45:33 -0400

    Add test for response

---
+++ spec/auth0_authorization/response_spec.rb
@@ -0,0 +1,39 @@
+module Auth0Authorization
+  RSpec.describe Response do
+    describe "#success?" do
+      it "returns true when response code is 200" do
+        response = Response.new(OpenStruct.new(code: "200"))
+
+        expect(response).to be_success
+      end
+
+      it "returns false otherwise" do
+        response = Response.new(OpenStruct.new(code: "403"))
+
+        expect(response).not_to be_success
+      end
+    end
+
+    describe "#body" do
+      it "returns deserialized JSON" do
+        response = Response.new(OpenStruct.new(body: JSON.dump({ hello: "world" })))
+
+        expect(response.body).to include("hello" => "world")
+      end
+
+      context "when body is not JSON" do
+        it "returns the body" do
+          response = Response.new(OpenStruct.new(body: JSON.dump("")))
+
+          expect(response.body).to eq("")
+        end
+
+        it "returns the body as is" do
+          response = Response.new(OpenStruct.new(body: "Not JSON"))
+
+          expect(response.body).to eq("Not JSON")
+        end
+      end
+    end
+  end
+end
