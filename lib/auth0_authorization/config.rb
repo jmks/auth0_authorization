4e79e9408240440a6a8682496aeb3d6942f803e1
Jason Schweier <jasons@nulogy.com>
2021-05-28 11:13:19 -0400

    Fix attribute name

--- lib/auth0_authorization/config.rb
+++ lib/auth0_authorization/config.rb
@@ -1,6 +1,6 @@
 module Auth0Authorization
   class Config
-    attr_writer :auth0_url, :extension_url, :client_id, :client_secret, :api_identifier
+    attr_accessor :auth_url, :extension_url, :client_id, :client_secret, :api_identifier
 
     def update(options)
       options.each_pair { |key, value| public_send("#{key}=", value) }
